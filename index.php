a7e362403f6a31d278aa972b9a73d71963416eb8
connmatt <matt.connolly1225@gmail.com>
2019-09-23 14:50:48 -0700

    more messing around

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
-<link rel="import" href="header.html">
+<?php
+        include("header.html")
+?>
         <!-- add Carousel component -->
         <br /><br />
         <a class="btn btn-outline-primary" href="" role="button"> Resume </a>
 
         <br /><br />
         <hr>
+
